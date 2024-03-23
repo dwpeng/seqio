83d082cf40ddc8845320ba4517e352b0946598b8
dwpeng <dwpeng1122@foxmail.com>
2024-03-23 16:55:56 +0800

    build by `pip wheel`

--- build-python.sh
+++ build-python.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -ex
-python3 setup.py sdist bdist_wheel
+python3 -m pip wheel .
+if [ -d dist ]; then
+    rm -rf dist
+fi
+mkdir dist
+mv *.whl dist/
 rm -rf build
 rm -rf python/src/*.egg-info
