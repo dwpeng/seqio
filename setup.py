03831f7154dbeaa221a182042d12a49ff30ccd26
dwpeng <dwpeng1122@foxmail.com>
2024-12-12 22:01:39 +0800

    feat: add pyproject.toml for project metadata and update setup.py with license and URL

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ extensions = [
         name="_fastseqio",
         sources=["./python/fastseqio.cc", "seqio.c"],
         include_dirs=[".", "python/pybind11/include"],
-        extra_compile_args=[""],
-        extra_link_args=["-lz"],
+        extra_compile_args=[],
+        extra_link_args=["-lz", "-lm"],
     )
 ]
 
@@ -15,6 +15,8 @@ setuptools.setup(
     version="0.0.3",
     author="dwpeng",
     author_email="1732889554@qq.com",
+    license="MIT",
+    url="https://github.com/dwpeng/fastseqio",
     description="A package for reading and writing fasta/fastq files",
     packages=setuptools.find_namespace_packages(where="./python/src"),
     package_dir={"": "./python/src"},
